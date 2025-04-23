14a6d9c9d539f4f62022400fde75d66083dff290
1109pgg <126872397+1109pgg@users.noreply.github.com>
2025-04-23 15:29:06 +0800

    Add files via upload

---
+++ 12/demo/src/main/java/com/example/demo/demos/web/config/LoginConfig.java
@@ -0,0 +1,37 @@
+package com.example.demo.demos.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class LoginConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/hello").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin();
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("test")
+                .password("123456")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
